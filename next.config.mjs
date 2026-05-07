31cc99f7049ac22288225fcc7e528231237ad687
Natun-Bikash-Chakma <natunbikashc67@gmail.com>
2026-05-08 03:50:36 +0900

    fix: updated basepath and assetPrefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/B13-A7-keen-keeper' : '',
-  assetPrefix: isProd ? '/B13-A7-keen-keeper/' : '',
+  basePath: '/B13-A7-keen-keeper',
+  assetPrefix: '/B13-A7-keen-keeper', 
   images: {
     unoptimized: true,
   },
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}", // যদি app ডিরেক্টরি বাইরে থাকে
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
