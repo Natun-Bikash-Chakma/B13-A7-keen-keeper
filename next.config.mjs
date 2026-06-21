dd9630dc1608a0d6e70fe3a0d554aa63037ef5eb
Natun-Bikash-Chakma <natunbikashc67@gmail.com>
2026-06-21 16:18:40 +0900

    fix: deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // এটি আপনার প্রোজেক্টকে স্ট্যাটিক HTML-এ রূপান্তর করবে
+  basePath: '/B13-A7-keen-keeper', // আপনার GitHub রেপোজিটরির নাম
   images: {
-    unoptimized: true,
+    unoptimized: true, // স্ট্যাটিক এক্সপোর্টের জন্য ইমেজ অপ্টিমাইজেশন বন্ধ করতে হবে
   },
-  output: 'export',
-  basePath: '/B13-A7-keen-keeper',
-  assetPrefix: '/B13-A7-keen-keeper',
 };
 
 export default nextConfig;
